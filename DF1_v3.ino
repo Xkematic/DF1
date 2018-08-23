21b6cfcf7b2db18ff7e7029ab77d14784ff0aa61
Xkematic <42655747+Xkematic@users.noreply.github.com>
2018-08-23 21:53:30 +0200

    First Code. Check to read MPU
    Reading MPU by I2C wiht stm32

---
+++ DF1_v3.ino
@@ -0,0 +1,44 @@
+
+// MPU Library
+#include<Wire.h>
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; // declare accellerometer and gyro variables TWO Bytes each
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin(); // initiate i2c system
+  Wire.beginTransmission(MPU_addr); // be sure we talk to our MPU vs some other device
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true); // done talking over to MPU device, for the moment
+  Serial.begin(9600); // initialize serial port to 9600 bps so you can see your debug messages in Arduino IDE via debug channel
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Wire.beginTransmission(MPU_addr); // get ready to talk to MPU again
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false); // done talking to MPU for the time being
+  Wire.requestFrom(MPU_addr,14);  // request a total of 14 registers
+  // all the fancy <<8| stuff is to bit shift the first 8 bits to
+  // the left & combine it with the next 8 bits to form 16 bits
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  // the above lines have gathered Accellerometer values for X, Y, Z
+  //  as well as Gyroscope values for X, Y, Z
+
+  Serial.print("AcX = "); Serial.print(AcX); // share accellerometer values over debug channel 
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX); // share gyroscope values over debug channel
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  delay(333); // delay a bit to not overwhelm you the user/programmer as you view the results
+
+}
