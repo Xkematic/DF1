e172e56ba3e5e0dc0438ad9957c2c139589d24a2
Xkematic <42655747+Xkematic@users.noreply.github.com>
2018-09-17 22:04:58 +0200

    Empty library for controlling MPU6050

---
+++ MPU6050.cpp
@@ -0,0 +1,9 @@
+/*
+  MPU6050.h - Library for controllin MPU6050.
+  Created by info@xkematic.com (Juan Guerrero and Manolo Madueño), September , 2018.
+  Released into the public domain under Creative Common License.
+*/
+
+#include "Arduino.h"
+#include "MPU6050.h"
+
---
+++ MPU6050.h
@@ -0,0 +1,19 @@
+/*
+  MPU6050.h - Library for controllin MPU6050.
+  Created by info@xkematic.com (Juan Guerrero and Manolo Madueño), September , 2018.
+  Released into the public domain under Creative Common License.
+*/
+#ifndef MPU6050_h
+#define MPU6050_h
+
+#include "Arduino.h"
+
+class MPU6050
+{
+  public:
+    
+  private:
+    
+};
+
+#endif
